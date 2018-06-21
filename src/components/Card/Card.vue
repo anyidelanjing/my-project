6c7e5fd3e768a17e5bb8f7df531a420055255f4e
lilian <1425183223@qq.com>
2018-06-21 09:40:39 +0800

    kk

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="book-card" >
+      <div class="thumb">
+          <img :src="book.image" alt="" class='img' mode='aspectFit'>
+      </div>
+      <div class="detail">
+          <div class="row">
+              <div class="right">
+                  {{book.rate}} <star :value='book.rate'></star>
+              </div>
+              <div class="left">
+                  {{book.title}}
+              </div>
+          </div>
+           <div class="row">
+              <div class="right">
+                   浏览量：
+              </div>
+              <div class="left">
+                  {{book.author}}
+              </div>
+          </div>
+           <div class="row">
+              <div class="right">
+                  添加人：
+              </div>
+              <div class="left">
+                  {{book.publisher}}
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import star from '@/components/star/star'
+    export default {
+        // created() {
+        //     console.log(book)
+        // },
+        props:{
+            book:{
+                type: Object
+            }
+        },
+        components:{star}
+    }
+</script>
+<style lang='stylus' rel='stylesheet/stylus' >
+    .book-card
+        padding: 5px
+        overflow: hidden
+        margin-top 5px
+        margin-bottom 5px
+        font-size 14px
+        .thumb
+            width: 90px
+            height 90px
+            float left 
+            margin: 0 auto
+            overflow hidden
+            .img
+                max-width 100%
+                max-height 100%
+        .detail
+            margin-left 100px
+            .row
+                line-height 20px
+                margin-bottom 3px
+                // color: #ea5149
+                .right
+                    float: right
+                .left
+                    margin-right 80px    
+
+</style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="rate">
+        <span>☆☆☆☆☆</span>
+        <div class="hollow" :style='style'>★★★★★</div>
+    </div>
+</template>
+<script type='text/ecmascript-6'>
+    export default {
+        props: {
+            value: {
+                type: Number,
+                default: 0
+            }
+        },
+        computed: {
+            style() {
+                return `width:${parseInt(this.value)/2-0.5}em`
+            }
+        }
+    }
+</script>
+<style lang='stylus' rel='stylesheet/stylus'>
+    .rate
+        position relative
+        display inline-block
+        color: #ea5149
+        .hollow
+            position absolute
+            display inline-block
+            top 0
+            left 0
+            width 0
+            overflow hidden
+
+</style>
